f6b9fe8b12af1ddd0626934bbca2f4b8943aa19d
Hiren Kumar <hirenkr@gmail.com>
2024-04-02 01:49:31 -0700

    Update tic_tac_toe.py

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,5 +1,7 @@
 
 
 print("Welcome to Tic Tac Toe")
-player1 = input("Enter your name")
-player2 = input("Enter your name")
+player1 = input("Enter your name: ")
+player2 = input("Enter your name: ")
+
+print("Welcome " + player1 + " and " + player2)
