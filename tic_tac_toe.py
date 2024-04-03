e17a06303868e9095f8fd9e11d959cb1899f12a9
Hiren Kumar <hirenkr@gmail.com>
2024-04-03 01:31:12 -0700

    choose

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,7 +1,9 @@
 
+import random
 
 print("Welcome to Tic Tac Toe")
 player1 = input("Enter your name: ")
 player2 = input("Enter your name: ")
 
 print("Welcome " + player1 + " and " + player2)
+order = input("Let's play: \nwho wants to go first? type 1 for {a}, 2 for {b}".format(a=player1,b=player2))
